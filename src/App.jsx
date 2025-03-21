181af4c1cc7bf063672f00462e3985b6e2ad2f84
Matt Buchs <110405782+MattBuchs@users.noreply.github.com>
2025-03-21 20:46:31 +0100

    add react-spinners for loading indicators and implement lazy loading in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,31 @@
 import { Routes, Route, BrowserRouter } from "react-router-dom";
-import Home from "./pages/Home";
-import Projects from "./pages/Projects";
-import Project from "./pages/Project";
-import Contact from "./pages/Contact";
-import Error from "./pages/Error";
+import { Suspense, lazy } from "react";
+import ScaleLoader from "react-spinners/ScaleLoader";
+
+const Home = lazy(() => import("./pages/Home"));
+const Projects = lazy(() => import("./pages/Projects"));
+const Project = lazy(() => import("./pages/Project"));
+const Contact = lazy(() => import("./pages/Contact"));
+const Error = lazy(() => import("./pages/Error"));
 
 function App() {
     return (
         <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/projects" element={<Projects />} />
-                <Route path="/project/:slug" element={<Project />} />
-                <Route path="/contact" element={<Contact />} />
-                <Route path="*" element={<Error />} />
-            </Routes>
+            <Suspense
+                fallback={
+                    <div className="flex justify-center items-center h-screen">
+                        <ScaleLoader color="#000" size={30} />
+                    </div>
+                }
+            >
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/projects" element={<Projects />} />
+                    <Route path="/project/:slug" element={<Project />} />
+                    <Route path="/contact" element={<Contact />} />
+                    <Route path="*" element={<Error />} />
+                </Routes>
+            </Suspense>
         </BrowserRouter>
     );
 }
